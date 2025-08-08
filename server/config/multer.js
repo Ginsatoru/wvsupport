eb5ac65e4ec07bbfe127202e35bdf987fecd39fa
Bo <bonai@gmail.com>
2025-08-08 10:54:22 +0700

    updated

---
+++ server/config/multer.js
@@ -0,0 +1,38 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const setupMulter = () => {
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      const uploadDir = path.join(__dirname, "../uploads");
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+      cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      const ext = path.extname(file.originalname);
+      cb(null, "team-" + uniqueSuffix + ext);
+    },
+  });
+
+  const upload = multer({
+    storage,
+    limits: {
+      fileSize: 5 * 1024 * 1024, // 5MB limit
+    },
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype.startsWith("image/")) {
+        cb(null, true);
+      } else {
+        cb(new Error("Only image files are allowed!"), false);
+      }
+    },
+  });
+
+  return upload;
+};
+
+module.exports = { setupMulter };
